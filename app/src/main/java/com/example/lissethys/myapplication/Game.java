77c9a6f72e8eca0b157ae4788139501a5aeac06e
Liam Thys <liam.thys@student.ucll.be>
2018-11-24 21:18:00 +0100

    da

--- app/src/main/java/com/example/lissethys/myapplication/Game.java
+++ app/src/main/java/com/example/lissethys/myapplication/Game.java
@@ -5,6 +5,16 @@ public class Game {
     private Dice dice2 = new Dice();
     private Dice dice3 = new Dice();
 
-
+    public void rollDices(){
+        if(!dice1.isStuck()){
+            dice1.rollDice();
+        }
+        if(!dice2.isStuck()){
+            dice2.rollDice();
+        }
+        if(!dice3.isStuck()){
+            dice3.rollDice();
+        }
+    }
 
 }
