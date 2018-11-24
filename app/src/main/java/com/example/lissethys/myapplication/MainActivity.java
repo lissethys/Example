642f23cfad4111c7e11f936b8ad951778863b1af
lissethys <r0448961@student.thomasmore.be>
2018-11-24 17:48:40 +0100

    dobbelsteen werkend

--- app/src/main/java/com/example/lissethys/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lissethys/myapplication/MainActivity.java
@@ -2,16 +2,46 @@ package com.example.lissethys.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Game game = new Game();
+    private Dice dice1 = new Dice();
+    private Dice dice2 = new Dice();
+    private Dice dice3 = new Dice();
+
+    private TextView mDiceTextView1;
+    private TextView mDiceTextView2;
+    private TextView mDiceTextView3;
+
+    private Button mButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        game.roll();
+
+        mDiceTextView1 = (TextView) findViewById(R.id.textView);
+        mDiceTextView2 = (TextView) findViewById(R.id.textView2);
+        mDiceTextView3 = (TextView) findViewById(R.id.textView3);
+
+        mButton = (Button) findViewById(R.id.button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dice1.rollDice();
+                dice2.rollDice();
+                dice3.rollDice();
+                mDiceTextView1.setText("" + dice1.getDice());
+                mDiceTextView2.setText("" + dice2.getDice());
+                mDiceTextView3.setText("" + dice3.getDice());
+            }
+        });
+
 
     }
 }
---
+++ app/src/main/java/com/example/lissethys/myapplication/main.java
@@ -0,0 +1,10 @@
+package com.example.lissethys.myapplication;
+
+public class main {
+    public static void main(String[] args) {
+        Dice dice = new Dice();
+
+        dice.rollDice();
+        System.out.println(dice.getDice());
+    }
+}
